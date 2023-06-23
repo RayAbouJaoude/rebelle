e162d5240a11ca09bd68e6f1b66e29d98522183e
User <user@Users-MacBook-Pro.local>
2023-06-23 22:21:33 +0300

    aa

--- application/controllers/Rebelle.php
+++ application/controllers/Rebelle.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Lb961 extends CI_Controller {
+class Rebelle extends CI_Controller {
 
 	public function __construct()
 	{
